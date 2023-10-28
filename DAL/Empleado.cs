0e6359321d62052664188b2333fb5cd155143dcf
MelissaMF <melissa.munoz1@virginiogomez.cl>
2023-10-28 20:02:34 -0300

    DAL

---
+++ DAL/Empleado.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Empleado
+    {
+        public Empleado() { }
+        public Empleado(int id, string nombre, string apellido, DateTime fechanacimiento, string departamento, float salario) {
+
+            this.ID = id;
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.FechaNacimiento = fechanacimiento;
+            this.Departamento = departamento;
+            this.Salario = salario;
+
+
+        }
+
+        public Empleado(string nombre, string apellido, DateTime fechanacimiento, string departamento, float salario)
+        {
+
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.FechaNacimiento = fechanacimiento;
+            this.Departamento = departamento;
+            this.Salario = salario;
+
+
+        }
+
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set;}
+        public DateTime FechaNacimiento { get; set;}
+        public string Departamento { get;set;}
+        public float Salario { get; set;}
+    
+    }
+}
