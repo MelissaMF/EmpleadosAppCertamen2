89a6e6bfa1d3b9878d4004a416dcc41b100be814
MelissaMF <melissa.munoz1@virginiogomez.cl>
2023-10-28 20:09:18 -0300

    DAL corregido

--- DAL/DatabaseHelper.cs
+++ DAL/DatabaseHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,34 @@ namespace DAL
 {
     internal class DatabaseHelper
     {
+        private static DatabaseHelper instance = new DatabaseHelper();
+
+        public static DatabaseHelper Instance
+        {
+            get { return instance; }
+        }
+
+        private static string CONNECTION_STRING = "Data Source=MELISSAMF\\SQLEXPRESS; Initial Catalog=Estudiantes; Integrated Security=True";
+
+        public int ExecuteNonQuery(string query)
+        {
+            SqlConnection sqlConnection = new SqlConnection(CONNECTION_STRING);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            return cmd.ExecuteNonQuery();
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+
+            SqlConnection sqlConnection = new SqlConnection(CONNECTION_STRING);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
     }
 }
--- DAL/EmpleadoDAL.cs
+++ DAL/EmpleadoDAL.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class EmpleadoDAL
+    public class EmpleadoDAL
     {
+        public bool GuardarEmpleado(Empleado empleado)
+        {
+            string fechanac = empleado.FechaNacimiento.ToString("yyyy-MM-dd");
+            string query = $"INSERT INTO empleado(nombre, apellido, fechanacimiento, departamento, salario) VALUES ('{empleado.Nombre}', '{empleado.Apellido}', '{fechanac}', {empleado.Departamento}, {empleado.Salario})";
+            int rows = DatabaseHelper.Instance.ExecuteNonQuery(query);
+            if (rows == 0) {
+                return false;
+            }
+            return true;
+        }
+
+        public List<Empleado> ObtenerEmpleados()
+        {
+            string query = "SELECT * FROM empleado";
+            DataTable dt = DatabaseHelper.Instance.ExecuteQuery(query);
+            List<Empleado> empleados = new List<Empleado>();
+            foreach (DataRow row in dt.Rows) { 
+                Empleado e = new Empleado();
+                e.Nombre = row["nombre"].ToString();
+                e.Apellido = row["apellido"].ToString() ;
+                e.FechaNacimiento = DateTime.Parse(row["fechanacimiento"].ToString();
+                e.Departamento = row["departamento"].ToString();
+                e.Salario = float.Parse(row["salario"].ToString());
+                empleados.Add(e);
+            }
+            return empleados;
+        }
+
+        public bool EliminarEmpleado(int id)
+        {
+            string query = $"DELETE FROM empleado WHERE id = '{id}'";
+            int rows = DatabaseHelper.Instance.ExecuteNonQuery(query);
+            if (rows == 0)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
